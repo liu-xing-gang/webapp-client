482819562fdf2798331f7789c69e9b0654a70eeb
Liu Xinggang <44444536@163.com>
2018-07-09 19:21:59 +0800

    竞彩足球混合投注

--- app.js
+++ app.js
@@ -154,6 +154,137 @@ CcpClient.Common = (function () {
 })()
 
 
+CcpClient.Drawing = function () {
+    this.init.apply(this, arguments)
+}
+
+CcpClient.Drawing.prototype = {
+    // 初始化
+    init: function (id, data) {
+        this.id = id
+        this.data = data
+        this.points = []
+        this.winPoints = []
+        this.drawPoints = []
+        this.losePoints = []
+        this.baseX = 0
+        this.baseY = 0
+
+
+        this.autosize()
+        if(this.id != null)
+            this.c = document.getElementById(this.id)
+
+        this.c.width=560*this.autosize()
+        this.c.height=120*this.autosize()
+
+        for (var i = 0; i < this.c.width; i += this.c.width/10) {
+            for (var j = 0; j < this.c.height; j += this.c.height/3) {
+                var obj = {}
+                obj.x = i + 6*this.autosize()
+                obj.y = j + 6*this.autosize()
+                this.points.push(obj)
+            }
+        }
+        
+        // x轴长度基准单位，y轴基准长度单位
+        this.baseX = this.c.width/10
+        this.baseY = this.c.height/3
+        this.ctx = this.c.getContext('2d')
+        this.ctx.lineWidth = 1;
+        this.ctx.strokeStyle = '#e5e2d3';
+        this.ctx.beginPath()
+        this.drawImg('../../images/icon-circle-default.png', this.points, this.ctx)
+        this.drawImg('../../images/icon-circle-win.png', this.winPoints, this.ctx)
+        this.drawImg('../../images/icon-circle-draw.png', this.drawPoints, this.ctx)
+        this.drawImg('../../images/icon-circle-lose.png', this.losePoints, this.ctx)
+        var linePoints = this.appendPoints(this.data, 'time')
+        this.lineTo(linePoints, this.ctx)
+    },
+    // 自适应宽高
+    autosize: function () {
+        var deviceWidth = document.documentElement.clientWidth
+        if (deviceWidth >= 750) deviceWidth = 750
+        return deviceWidth / 7.5 / 100 
+    },
+    // 生成有序的坐标
+    appendPoints: function(arr, str){
+        if(arr.length > 10) return
+
+        // 转换成时间戳进行比较
+        arr.forEach(function(item){
+            item.time = new Date(item.time).getTime()
+        })
+
+        var _points = []
+        // 通过排序得到x轴坐标位置
+        this.sort(arr, 'time')
+        for (var i = 0; i <arr.length; i ++) {
+            var obj = {}
+            obj.x = (6*this.autosize())+this.baseX*i
+            // 根据“胜，平，负”得到y的坐标
+            if(arr[i].endType === 1){
+                obj.y = 6*this.autosize()
+                this.winPoints.push(obj)
+            } else if(arr[i].endType === 0){
+                obj.y = (6*this.autosize())+this.baseY
+                this.drawPoints.push(obj)
+            } else {
+                obj.y = (6*this.autosize())+this.baseY*2
+                this.losePoints.push(obj)
+            }
+            _points.push(obj)
+        }
+        return _points
+    },
+    // 绘制圆点
+    drawImg: function (src, points, ctx){
+        var img = new Image()
+        img.src = src
+        try{
+            img.addEventListener('load' , function(){
+                for (var i = 0; i < points.length; i++) {
+                    var point = points[i]
+                    ctx.moveTo(point.x, point.y)
+                    ctx.drawImage(img, point.x, point.y);
+                }
+            }, false)
+        }
+        catch(e){
+        }
+    },
+    // 需要根据x坐标进行一次排序，然后串联起来
+    sort: function (arr, key){
+        for (var i = 1; i < arr.length; i++) {
+            var temp = arr[i];
+            var j = i - 1;
+            while (j >= 0 && arr[j][key] > temp[key]) {
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                j--
+            }
+            arr[j + 1] = temp
+        }
+        return arr
+    },
+    // 以x轴大小递增画连线
+    lineTo: function (sortLinePoints, ctx){
+        // var linePoints =  winPoints.concat(drawPoints).concat(losePoints)
+        // var sortLinePoints = sort(linePoints, 'x')
+        // console.log(sortLinePoints)
+        for(var i = 0; i < sortLinePoints.length; i ++){
+            var linePoint = sortLinePoints[i]
+            if(i === 0)
+                ctx.moveTo(linePoint.x, linePoint.y)
+            ctx.lineTo(linePoint.x+3, linePoint.y+3)
+        }
+        ctx.stroke();
+    }
+
+
+}
+
+
 // 调用
 // CcpClient.Common.getInstance().pubMethod1()
 CcpClient.Common.getInstance().init()
--- component/component.js
+++ component/component.js
@@ -23,3 +23,67 @@ Vue.component('screen-layer', {
     },
     
 })
+
+/**
+ * ========================================
+ * 组件名称: layer-substance
+ * ========================================
+ * 功能说明: 竞彩足球弹层
+ * ========================================
+ * 作者    : Liu Xinggang
+ * ======================================== 
+ */
+Vue.component('layer-substance', {
+    props: {
+        showlayer: false,
+        match: Object,
+        chosen: Array,
+        chosenlist: Array,
+    },
+    data: function() {
+        return {
+            showlayer: this.showlayer,
+            chosen: this.chosen,
+            chosenlist: this.chosenlist,
+        }
+    },
+    template: '#test',
+    methods: {
+        closeLayer: function() {
+            this.showlayer = false
+            this.$emit('closelayer', this.showlayer)
+        },
+        pitched: function (item, flag, o) {
+
+            // 根据场次id获得已选几场
+            function unique(key, arr){
+                var i,j
+                for(i=0; i<arr.length; i++){
+                    for(j = arr.length - 1; j >= i+1; j --)
+                    {
+                        if(arr[i][key] === arr[j][key])
+                            arr.splice(i, 1)
+                    }
+                }
+            }
+
+            this.chosen.push(o)
+            unique('competitionNumber', this.chosen)
+            console.log(this.chosenlist)
+
+            //使已选择弹出层，保持始终居中对其，而整理数据格式
+            this.chosenlist.push(item)
+            
+            item[flag] = !item[flag]
+            item[flag] === true ? o.chosenNum ++ : o.chosenNum --  
+            o.chosenNum = o.chosenNum <= 0 ? 0 : o.chosenNum
+            // 是否选择
+            o.chosenNum >0 ? o.isChosen = true : o.isChosen = false
+            // 切换显示底部提示
+            if(o.chosenNum === 0){
+                this.chosen.splice(0, this.chosen.length)
+                this.chosenlist.splice(0, this.chosenlist.length)
+            }
+        }
+    }
+})
